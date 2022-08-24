83ae10cbd3f6265149e79df52408b9e20458c494
Weronika Kuprel <ficekw@icloud.com>
2022-08-24 10:38:22 +0200

    adding close overlays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  //tworzę state dla pokazywania lub ukrywania koszyka
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  //funkcja pokazująca
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  //funkcja ukrywająca
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+  //Cart daję w IF
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
