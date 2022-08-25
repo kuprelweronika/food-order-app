05e7d6e9d48c0c5ae331ded55154df0ac12ea43a
Weronika Kuprel <ficekw@icloud.com>
2022-08-25 12:11:05 +0200

    adding bump cart when usr add sth to cart - finish project

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,10 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   //wyciagamy ilość rzeczy wkoszyku
   //reduce pozwala zmienić array na single value
@@ -13,8 +14,38 @@ const HeaderCartButton = (props) => {
     return curNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  //lacze dwie klasy w jedna, wiec domyslnie teraz caly czas
+  //przycisk robi bump
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  //zeby robil bump za kazdym dodaniem elementu do koszyka uzywam
+  //useEffect
+
+  useEffect(() => {
+    //chcemy zmieniac button class - dodawac i usuwac
+    //tu bedzie timer ktory bedzie usuwal
+    //musimy zaimportowac takze useState
+
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]); //ma sie wywolac zawsze jaksie zmieni cartCtx -> items
+  //to powoduje ze sie wywola tylko raz, wiec dodajemy seTimeout
+  //trzeba cleanup ten Timer iec dodaje funkcje czyszczaca
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
